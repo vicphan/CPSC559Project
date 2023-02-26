32602fc2b36be7ce55f4080a0a3d9c08ba5156bb
Desiree Leal <desiree.leal1@ucalgary.ca>
2023-02-25 21:35:04 -0700

    connection to mongodb

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -1,9 +1,12 @@
-import { DB_HOST, DB_PORT, DB_DATABASE } from '@config';
+import { MONGO_URI } from '@config';
 
 export const dbConnection = {
-  url: `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`,
+  url: MONGO_URI,
+  
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true
   },
 };
+
+console.log(MONGO_URI);
