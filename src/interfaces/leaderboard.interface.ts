56129b65e5e982333f8c54e1df5478fb84655efb
Desiree Leal <desiree.leal1@ucalgary.ca>
2023-02-28 02:32:25 -0700

    deleted leaaderboard interface that is no longer used

--- src/interfaces/leaderboard.interface.ts
+++
@@ -1,8 +0,0 @@
-import { Game } from '@interfaces/games.interface';
-import { Player } from '@interfaces/players.interface';
-
-export interface Leaderboard {
-    _id: string;
-    game: Game;
-    leaders: Array<string>,
-}
