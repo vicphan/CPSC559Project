7545d075d895cae17e8dcef42d281f23b2ebc937
JeromeGobeil <jerome.gobeil@gmail.com>
2023-02-27 18:01:23 -0700

    Load questions on startup

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,22 @@ import UsersRoute from '@routes/users.route';
 import GamesRoute from '@routes/games.route';
 import PlayersRoute from '@routes/players.route';
 import validateEnv from '@utils/validateEnv';
+import questionModel from '@models/question.model';
 
 validateEnv();
 
 const app = new App([new IndexRoute(), new UsersRoute(), new AuthRoute(), new GamesRoute(), new PlayersRoute()]);
+
+const { readFileSync } = require('fs');
+const data = readFileSync('./questions.json');
+const questionList = JSON.parse(data);
+for (var i =0; i < questionList["questions"].length; i++)
+{
+    questionModel.create({ 
+        prompt: questionList["questions"][i]["question"], 
+        correctAnswerIndex: questionList["questions"][i]["correctIndex"], 
+        answers: questionList["questions"][i]["answers"] });
+}
+
 
 app.listen();
