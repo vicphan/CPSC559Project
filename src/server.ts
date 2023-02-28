af6cf26e0b7d6b31a7958cba38c3d46780a9b943
JeromeGobeil <jerome.gobeil@gmail.com>
2023-02-27 18:18:26 -0700

    Only create questions if they dont already exist

--- src/server.ts
+++ src/server.ts
@@ -7,20 +7,28 @@ import PlayersRoute from '@routes/players.route';
 import validateEnv from '@utils/validateEnv';
 import questionModel from '@models/question.model';
 
+async function createQuestions()
+{
+    const modelCount = (await questionModel.find()).length;
+    if (modelCount == 0)
+    {
+        const { readFileSync } = require('fs');
+        const data = readFileSync('./questions.json');
+        const questionList = JSON.parse(data);
+        for (var i =0; i < questionList["questions"].length; i++)
+        {
+            questionModel.create({ 
+            prompt: questionList["questions"][i]["question"], 
+            correctAnswerIndex: questionList["questions"][i]["correctIndex"], 
+            answers: questionList["questions"][i]["answers"] });
+        }
+    }
+}
+
 validateEnv();
 
 const app = new App([new IndexRoute(), new UsersRoute(), new AuthRoute(), new GamesRoute(), new PlayersRoute()]);
 
-const { readFileSync } = require('fs');
-const data = readFileSync('./questions.json');
-const questionList = JSON.parse(data);
-for (var i =0; i < questionList["questions"].length; i++)
-{
-    questionModel.create({ 
-        prompt: questionList["questions"][i]["question"], 
-        correctAnswerIndex: questionList["questions"][i]["correctIndex"], 
-        answers: questionList["questions"][i]["answers"] });
-}
-
+createQuestions();
 
 app.listen();
