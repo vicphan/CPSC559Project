8644ef9d841bfd45a624fa2b085abf44ed04e555
Jhappy77 <joel.happ@shaw.ca>
2023-03-01 00:16:44 -0700

    Install socketio, fix prettier issue

---
+++ src/sockets/socket.ts
@@ -0,0 +1,10 @@
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', socket => {
+  console.log('a user connected');
+});
