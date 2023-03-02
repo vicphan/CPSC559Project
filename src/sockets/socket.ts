5dd0afa2c9e5265fad0d698bfef41608f41e186c
Jhappy77 <joel.happ@shaw.ca>
2023-03-02 02:15:00 -0700

    Add socket connectivity

--- src/sockets/socket.ts
+++ src/sockets/socket.ts
@@ -1,10 +1,50 @@
-const { Server } = require('socket.io');
-const io = new Server(server);
+import { Application } from 'express';
+import { Socket, Server } from 'socket.io';
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+const http = require('http');
 
-io.on('connection', socket => {
-  console.log('a user connected');
-});
+const getHostRoom = (gameCode: string) => `${gameCode}-host`;
+interface JoinGameRoomPayload {
+  gameCode: string;
+  playerName: string;
+}
+
+export function initializeSocket(app: Application) {
+  const server = http.createServer(app);
+  const io = new Server(server, {
+    cors: {
+      origin: 'http://localhost:3001',
+      methods: ['GET', 'POST'],
+    },
+  });
+
+  io.on('connection', (socket: Socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('a user disconnected!');
+    });
+
+    socket.on('join-game-room', async ({ gameCode, playerName }: JoinGameRoomPayload) => {
+      console.log('Join game room!');
+      if (!!gameCode) {
+        console.log(`Joined room ${gameCode}. ${playerName}`);
+        socket.join(gameCode);
+        socket.data['playerName'] = playerName;
+        const hostRoom = getHostRoom(gameCode);
+        const players = await []; // await getPlayersFromRoom(gameCode);
+        io.to(hostRoom).emit('players-list', players);
+      }
+    });
+
+    socket.on('join-game-room-as-host', (code?: string) => {
+      if (!!code) {
+        console.log(`Host joined room ${code}`);
+        socket.join(code);
+      }
+    });
+  });
+
+  server.listen(8008, () => {
+    console.log('Socket listening on Port 8008');
+  });
+}
