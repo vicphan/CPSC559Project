2435c538ba075a9ba3a23a6d8a2f097bd24ca392
Jhappy77 <joel.happ@shaw.ca>
2023-03-02 22:38:17 -0700

    Remove physical winston logs

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -7,11 +7,11 @@ import winstonDaily from 'winston-daily-rotate-file';
 const LOG_DIR = 'logs';
 
 // logs dir
-const logDir: string = join(__dirname, LOG_DIR);
+// const logDir: string = join(__dirname, LOG_DIR);
 
-if (!existsSync(logDir)) {
-  mkdirSync(logDir);
-}
+// if (!existsSync(logDir)) {
+//   mkdirSync(logDir);
+// }
 
 // Define log format
 const logFormat = winston.format.printf(({ timestamp, level, message }) => `${timestamp} ${level}: ${message}`);
@@ -27,29 +27,29 @@ const logger = winston.createLogger({
     }),
     logFormat,
   ),
-  transports: [
-    // debug log setting
-    new winstonDaily({
-      level: 'debug',
-      datePattern: 'YYYY-MM-DD',
-      dirname: logDir + '/debug', // log file /logs/debug/*.log in save
-      filename: `%DATE%.log`,
-      maxFiles: 30, // 30 Days saved
-      json: false,
-      zippedArchive: true,
-    }),
-    // error log setting
-    new winstonDaily({
-      level: 'error',
-      datePattern: 'YYYY-MM-DD',
-      dirname: logDir + '/error', // log file /logs/error/*.log in save
-      filename: `%DATE%.log`,
-      maxFiles: 30, // 30 Days saved
-      handleExceptions: true,
-      json: false,
-      zippedArchive: true,
-    }),
-  ],
+  // transports: [
+  //   // debug log setting
+  //   new winstonDaily({
+  //     level: 'debug',
+  //     datePattern: 'YYYY-MM-DD',
+  //     dirname: logDir + '/debug', // log file /logs/debug/*.log in save
+  //     filename: `%DATE%.log`,
+  //     maxFiles: 30, // 30 Days saved
+  //     json: false,
+  //     zippedArchive: true,
+  //   }),
+  //   // error log setting
+  //   new winstonDaily({
+  //     level: 'error',
+  //     datePattern: 'YYYY-MM-DD',
+  //     dirname: logDir + '/error', // log file /logs/error/*.log in save
+  //     filename: `%DATE%.log`,
+  //     maxFiles: 30, // 30 Days saved
+  //     handleExceptions: true,
+  //     json: false,
+  //     zippedArchive: true,
+  //   }),
+  // ],
 });
 
 logger.add(
