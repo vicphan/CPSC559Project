1db7d5ebd7a01403d73513b1d3c89ce598687978
Joel <45266424+Jhappy77@users.noreply.github.com>
2023-03-10 13:09:12 -0700

    Move questions to ts instead of json

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,11 @@ import GamesRoute from './routes/games.route';
 import PlayersRoute from './routes/players.route';
 import questionModel from './models/question.model';
 import App from './app';
-import { createQuestions } from './createQuestions';
+import { createQuestions } from './utils/createQuestions';
 
 // validateEnv();
 // createQuestions();
 const app = new App([new IndexRoute(), new UsersRoute(), new AuthRoute(), new GamesRoute(), new PlayersRoute()]);
-// createQuestions();
+createQuestions();
 
 app.listen();
