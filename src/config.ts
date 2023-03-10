73942c9fd82ad2d37414196e822a2342a49f7716
Joel <45266424+Jhappy77@users.noreply.github.com>
2023-03-10 11:41:08 -0700

    Revert "Fix issue with questions not working"

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,2 @@
-import { config } from 'dotenv';
-config();
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
 export const { NODE_ENV, PORT, MONGO_URI, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN } = process.env;
--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,28 @@ import IndexRoute from './routes/index.route';
 import UsersRoute from './routes/users.route';
 import GamesRoute from './routes/games.route';
 import PlayersRoute from './routes/players.route';
+import questionModel from './models/question.model';
 import App from './app';
-import { createQuestions } from './utils/createQuestions';
+
+async function createQuestions() {
+  const modelCount = (await questionModel.find()).length;
+  if (modelCount == 0) {
+    const { readFileSync } = require('fs');
+    const data = readFileSync('./questions.json');
+    const questionList = JSON.parse(data);
+    for (let i = 0; i < questionList['questions'].length; i++) {
+      questionModel.create({
+        prompt: questionList['questions'][i]['question'],
+        correctAnswerIndex: questionList['questions'][i]['correctIndex'],
+        answers: questionList['questions'][i]['answers'],
+        index: i,
+      });
+    }
+  }
+}
 
 // validateEnv();
-createQuestions();
 const app = new App([new IndexRoute(), new UsersRoute(), new AuthRoute(), new GamesRoute(), new PlayersRoute()]);
+createQuestions();
 
 app.listen();
